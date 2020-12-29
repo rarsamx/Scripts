176fa9efc4464b50878b999929f5406f337d9594
Rarsa <rarsa@yahoo.com>
2020-12-29 17:00:49 -0500

    Added mbrBackup as counterpart of mbrRestore

---
+++ bash/mbrBackup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Creates a backup of the mbr in a disk
+#
+# Author: Raul Suarez
+# https://www.usingfoss.com/
+# License: GPL v3.0
+
+[ "$(id -u)" -eq "0" ] || { echo "Please run as root or with sudo"; exit 1; }
+
+usage=$(echo -e "\nusage: \033[1;32m${0##*/} mode device backupFile\033[0m\nWhere:\n  mode: boot|full\n  device: \\dev\\xxx\n  backupFile: Fully qualified file name")
+
+MODE=${1?"${usage}"}
+DEVICE=${2?"${usage}"}
+BACKUPFILE=${3?"${usage}"}
+
+case "$MODE" in 
+	full) dd if=${DEVICE} of=${BACKUPFILE} bs=512 count=1
+				;;
+	boot) dd if=${DEVICE} of=${BACKUPFILE} bs=466 count=1
+				;;
+     *) echo -e "ERROR: mode should be \033[1;32mfull\033[0m or \033[1;32mboot\033[0m"
+        ;;
+esac
