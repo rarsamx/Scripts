b012cee1c6c8a36795b1f2298b66e08dfc29032c
Rarsa <rarsa@yahoo.com>
2022-03-23 22:30:40 -0400

    Replaced with xmbGetCPUTemp.sh which writes to a named pipe

--- bash/getCPUTemp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# xmobar helper to get the cpu temperature
-#Author: Raul Suarez
-#https://www.usingfoss.com/
-#License: GPL v3.0
-
-sensors | sed -n -e 's/CPU Temperature:[[:space:]]*+\([0-9]*\).*/\1/p'
